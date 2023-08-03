e0968affe28e0fa5c10629e2a1652f880a0a5836
Robert Richter <robertrichter87@web.de>
2023-08-03 17:07:22 +0200

    Update 1.2.1

--- home/start_mouse_click.sh
+++ home/start_mouse_click.sh
@@ -3,10 +3,12 @@
 export DISPLAY=:0.0
 
 # Click on web page to activate webaudio auto-play
-monitor_x="$(fbset -s | awk '$1 == "geometry" { print $2 }')"
+monitor_x="$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)"
 sub_x=100
-monitor_y="$(fbset -s | awk '$1 == "geometry" { print $3 }')"
+monitor_y="$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)"
 sub_y=50
+#echo $monitor_x
+#echo $monitor_y
 buttom_x=$(expr $monitor_x - $sub_x)
 buttom_y=$(expr $monitor_y - $sub_y)
 xdotool mousemove $buttom_x $buttom_y
