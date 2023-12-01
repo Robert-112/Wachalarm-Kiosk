6a433a032f8a45e80ef0273a27bc09366b9ca5ad
Robert Richter <robertrichter87@web.de>
2023-12-01 11:19:25 +0100

    Installationsskript verbessert

--- image-setup.sh
+++ image-setup.sh
@@ -71,7 +71,7 @@ cp -v "$BOOT_CMDLINE_TXT" "$BOOT_CMDLINE_TXT.backup"
 cp -v "$BOOT_CONFIG_TXT" "$BOOT_CONFIG_TXT.backup"
 
 working "Wachalarm-Einstellungsdatei hinterlegen"
-cp -v "home/wachalarm_einstellungen.txt" "$MOUNTED_BOOT_VOLUME/wachalarm_einstellungen.txt"
+cp -v "$DIR/home/wachalarm_einstellungen.txt" "$MOUNTED_BOOT_VOLUME/wachalarm_einstellungen.txt"
 
 working "Automatische Expansion der root-Partition deaktivieren"
 echo "Updating: $BOOT_CMDLINE_TXT"
@@ -82,7 +82,7 @@ working "SSH aktivieren"
 # https://www.raspberrypi.org/documentation/remote-access/ssh/
 touch "$MOUNTED_BOOT_VOLUME/ssh"
 
-working "Making boot quieter (part 1)" # https://scribles.net/customizing-boot-up-screen-on-raspberry-pi/
+working "Startvorgang anpassen" # https://scribles.net/customizing-boot-up-screen-on-raspberry-pi/
 echo "Updating: $BOOT_CONFIG_TXT"
 perl -i -p0e "s/#disable_overscan=1/disable_overscan=1/g" "$BOOT_CONFIG_TXT" # "perl" is more cross-platform than "sed -i"
 echo -e "\ndisable_splash=1" >> "$BOOT_CONFIG_TXT"
@@ -104,24 +104,25 @@ sudo umount "$mount_path"
 
 
 question "Raspberry Pi starten:"
-echo "* SD-Karte auswerfen & in Pi einsetzen (ggf. zuvor \"umount /media/sdkarte\""
+echo "* SD-Karte entfernen & in Pi einsetzen"
 echo "* Raspberry Pi mit Netzwerk verbinden"
 echo "* Pi booten / starten"
-echo "* es folgen viele Password-Abfragen fuer den SSH-Zugriff auf den Raspberry"
+echo "* es folgt eine Password-Abfrage fuer den SSH-Zugriff auf den Raspberry"
 echo "IP-Adresse eingeben:"
 read IP
 
-working "Installing temporary SSH pubkey (use \"sudo ssh-keygen -t rsa\" on server)"
-echo -e "Password hint: \"wachalarm\""
+working "SSH-Key des Servers wird auf Client hinterlegt"
+echo -e "(Hinweis 1: zuvor \"sudo ssh-keygen -t rsa\" auf dem Server ausfuehren)"
+echo -e "(Hinweis 2: Password \"wachalarm\")"
 ssh "mkdir .ssh && echo '$SSH_PUBKEY' > .ssh/authorized_keys"
 
-#working "Setting locale"
 # We want to do this as early as possible, so perl et al won't complain about misconfigured locales for the rest of the image prep
+working "Sprache einstellen"
 ssh "echo $LOCALE | sudo tee /etc/locale.gen"
 ssh "sudo locale-gen"
 ssh "echo -e \"LANGUAGE=$LANGUAGE\nLC_ALL=$LANGUAGE\" | sudo tee /etc/environment"
 
-#working "hostname festlegen"
+working "Hostname festlegen"
 # We want to do this right before reboot, so we don't get a lot of unnecessary complaints about "sudo: unable to resolve host chilipie-kiosk" (https://askubuntu.com/a/59517)
 ssh "sudo hostnamectl set-hostname wachalarm"
 ssh "sudo perl -i -p0e 's/raspberrypi/wachalarm/g' /etc/hosts" # "perl" is more cross-platform than "sed -i"
@@ -131,28 +132,28 @@ set +e
 
 # From raspi-config: https://github.com/RPi-Distro/raspi-config/blob/d98686647ced7c0c0490dc123432834735d1c13d/raspi-config#L1313-L1321
 # See also: https://github.com/futurice/chilipie-kiosk/issues/61#issuecomment-524622522
-working "auto-login aktiverento CLI"
+working "Auto-Login aktiveren"
 ssh "sudo systemctl set-default multi-user.target"
 ssh "sudo ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service"
 ssh "sudo mkdir -p /etc/systemd/system/getty@tty1.service.d"
 ssh "echo -e '[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin pi --noclear %I \$TERM\n' | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf"
 
-#working "Setting timezone"
+working "Zeitzone einstellen"
 ssh "(echo '$TIMEZONE' | sudo tee /etc/timezone) && sudo dpkg-reconfigure --frontend noninteractive tzdata"
 
 #working "Setting keyboard layout"
 #ssh "(echo -e 'XKBMODEL="pc105"\nXKBLAYOUT="$KEYBOARD"\nXKBVARIANT=""\nXKBOPTIONS=""\nBACKSPACE="guess"\n' | sudo tee /etc/default/keyboard) && sudo dpkg-reconfigure --frontend noninteractive keyboard-configuration"
 
-working "Silencing console logins" # this is to avoid a brief flash of the console login before X comes up
+working "Konsolen-Logins anpassen" # this is to avoid a brief flash of the console login before X comes up
 ssh "sudo rm /etc/profile.d/sshpwd.sh /etc/profile.d/wifi-check.sh" # remove warnings about default password and WiFi country (https://raspberrypi.stackexchange.com/a/105234)
 ssh "touch .hushlogin" # https://scribles.net/silent-boot-on-raspbian-stretch-in-console-mode/
 ssh "sudo perl -i -p0e 's#--autologin pi#--skip-login --noissue --login-options \"-f pi\"#g' /etc/systemd/system/getty@tty1.service.d/autologin.conf" # "perl" is more cross-platform than "sed -i"
 
-working "Installing packages"
+working "Pakete installieren"
 ssh "sudo apt-get update && sudo apt-get -y upgrade && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y vim matchbox-window-manager unclutter mailutils nitrogen jq chromium-browser xserver-xorg xinit rpd-plym-splash xdotool rng-tools xinput-calibrator cec-utils realvnc-vnc-server unattended-upgrades npm nodejs lshw xfonts-100dpi"
 # We install mailutils just so that you can check "mail" for cronjob output
 
-working "Setting home directory default content"
+working "Inhalt des home-Ordners uebertragen"
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -rp "home" "pi@$IP:/home/pi/"
 ssh "mv /home/pi/home/* /home/pi/"
 ssh "mv /home/pi/home/.bash_profile /home/pi/"
@@ -160,16 +161,16 @@ ssh "mv /home/pi/home/.xsession /home/pi/"
 ssh "mv /home/pi/home/.matchbox* /home/pi/"
 ssh "rm -r /home/pi/home"
 
-working "Skripts ausfuehrbar machen"
+working "Skripte ausfuehrbar machen"
 ssh "chmod +x *.sh && chmod +x .xsession"
 
-working "Setting splash screen background"
+working "Bild beim Startvorgang hinterlegen"
 ssh "sudo rm /usr/share/plymouth/themes/pix/splash.png && sudo ln -s /home/pi/background.png /usr/share/plymouth/themes/pix/splash.png"
 
-working "Installing default crontab"
+working "Crontab installieren"
 ssh "crontab /home/pi/crontab.example"
 
-working "VNC-Service einrichten (Kennwort \"wachalarm\")"
+working "VNC-Dienst einrichten (Kennwort \"wachalarm\")"
 ssh "sudo cp /home/pi/vncserver-x11 /root/.vnc/config.d/vncserver-x11"
 ssh "sudo systemctl enable vncserver-x11-serviced.service && sudo systemctl start vncserver-x11-serviced.service"
 
@@ -188,10 +189,11 @@ ssh "sudo cp /home/pi/hosts.deny /etc/hosts.deny && sudo cp /home/pi/hosts.allow
 working "Waip-Standby-Skript installieren"
 ssh "(cd /home/pi && npm install)"
 
-working "Rebooting the Pi"
+working "SSH-Key entfernen und Pi neustarten"
+ssh "rm .ssh/authorized_keys"
 ssh "sudo reboot"
 
-echo "Waiting for host to come back up..."
+echo "Warten auf Neustart des Raspberry-Pi"
 until SSH_CONNECT_TIMEOUT=5 ssh "echo OK"
 do
   sleep 1
